0ef7984919651bdc443d5035b8177bf9f692649c
Srihitha47 <srihitha0475@gmail.com>
2025-03-19 11:25:45 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export default function Footer() {
     return (
       <div className="App-Footer-row">
-        <div>@Copy right 2025.All rights reserved.</div>
+        <div><center>@Copy right 2025.All rights reserved.</center></div>
       </div>
     )
 }
