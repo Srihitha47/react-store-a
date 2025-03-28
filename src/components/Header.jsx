2b258b57a1b355368283c7d2826dba19d2abdef8
Srihitha47 <srihitha0475@gmail.com>
2025-03-28 14:10:04 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,24 @@
-import "./Header.css"
-import { Link } from "react-router-dom"
+import "./Header.css";
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { appContext } from "../App";
-export default function Header(){
-    const { user,} = useContext(appContext);
-    return(
-        <div className="App-Header-Row">
-            <div>React Store-[{user.name}]</div>
-            <div>
-                <Link to="home">Home</Link>|
-                <Link to="cart">Cart</Link>|
-                <Link to="login" >Login</Link>
-            </div>
-        </div>
-    )
+export default function Header() {
+  const { user, setUser } = useContext(appContext);
+  return (
+    <div className="App-Header-Row">
+      <div>React Store</div>
+      <div>
+        <Link to="home">Home</Link> |<Link to="cart">Cart</Link> |
+        {user.email === "" ? (
+          <Link to="login">Login</Link>
+        ) : (
+          <Link to="login" onClick={() => setUser({ ...user, email: "" })}>
+            Logout
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 }
 
 
